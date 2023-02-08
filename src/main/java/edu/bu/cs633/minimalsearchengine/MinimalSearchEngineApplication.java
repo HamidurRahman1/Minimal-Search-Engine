a6f0ecb83f04fd00821e62802f9cf6aa53ae68ca
Hamidur Rahman <Hamidur0405@gmail.com>
2023-02-07 20:01:30 -0500

    enabling MSE to run in the background when crawling URLs, logic to be implemented for crawling

--- src/main/java/edu/bu/cs633/minimalsearchengine/MinimalSearchEngineApplication.java
+++ src/main/java/edu/bu/cs633/minimalsearchengine/MinimalSearchEngineApplication.java
@@ -2,7 +2,9 @@ package edu.bu.cs633.minimalsearchengine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class MinimalSearchEngineApplication {
     public static void main( String[] args ) {
---
+++ src/main/java/edu/bu/cs633/minimalsearchengine/asyncjob/AsyncJobServiceImpl.java
@@ -0,0 +1,46 @@
+package edu.bu.cs633.minimalsearchengine.asyncjob;
+
+import edu.bu.cs633.minimalsearchengine.models.dao.Admin;
+import edu.bu.cs633.minimalsearchengine.services.AdminService;
+import edu.bu.cs633.minimalsearchengine.services.PageService;
+import edu.bu.cs633.minimalsearchengine.services.WordService;
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
+
+@Service
+public class AsyncJobServiceImpl implements AsyncJobService {
+
+    private final ApplicationContext applicationContext;
+    private final AdminService adminService;
+    private final PageService pageService;
+
+    private final WordService wordService;
+
+    @Autowired
+    public AsyncJobServiceImpl(ApplicationContext applicationContext, AdminService adminService,
+                               PageService pageService, WordService wordService) {
+
+        this.applicationContext = applicationContext;
+        this.adminService = adminService;
+        this.pageService = pageService;
+        this.wordService = wordService;
+    }
+
+    private static final Logger logger = Logger.getLogger(AsyncJobServiceImpl.class.getName());
+
+    @Override
+    @Async(value = "threadPoolTaskExecutor")
+    public void crawl(final Admin admin, final String url) {
+
+        logger.info("Job started to crawl URL= " + url + " by " + admin.getUsername());
+
+        // TBI
+
+        logger.info("Job finished.");
+    }
+
+}
